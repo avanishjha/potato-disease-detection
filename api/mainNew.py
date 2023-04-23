a03fac7031dbff88d15b449a9077769b9073f9f3
JusCray <avanishjha2011@gmail.com>
2023-04-23 12:14:08 +0530

    Front-end

---
+++ api/mainNew.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn 
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+# Add the CORS middleware to your application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model('../potatoes.h5')
+CLASS_NAMES = ['Early Blight','Late Blight', 'Healthy']
+
+@app.get('/ping')
+async def ping():
+    return "Hello"
+
+def read_file_as_image(data) -> np.ndarray:
+    image= np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post('/predict')
+async def predict (
+    file: UploadFile  = File(...)
+
+):
+    image= read_file_as_image(await file.read())
+    img_batch= np.expand_dims(image,0)
+    prediction= MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence= np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('upload-form');
+const fileInput = document.getElementById('file-input');
+const resultDiv = document.getElementById('result');
+const resultText = document.getElementById('result-text');
+const resultConfidence = document.getElementById('result-confidence');
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', fileInput.files[0]);
+
+    const response = await fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        body: formData
+    });
+
+    const data = await response.json();
+
+    resultText.innerText = data.class;
+    resultConfidence.innerText = `Confidence: ${data.confidence*100}%`;
+    const uploadedImage = document.getElementById('uploaded-image');
+    uploadedImage.src = URL.createObjectURL(fileInput.files[0]);
+
+
+    
+
+    resultDiv.classList.remove('hidden');
+});
+
+fileInput.addEventListener('change', () => {
+    const fileName = fileInput.files[0].name;
+    const fileLabel = document.getElementById('file-label');
+    fileLabel.innerText = fileName;
+});
+
+resultDiv.addEventListener('click', () => {
+    resultDiv.classList.add('hidden');
+    resultText.innerText = '';
+    resultConfidence.innerText = '';
+    fileInput.value = '';
+    document.getElementById('file-label').innerText = 'Choose an image';
+});
